5c89d743542b2685a5a1d012aaf3608b0e9f9646
Alessandro Delle Foglie <alessandro@MacBook-Pro-di-Alessandro.local>
2022-04-22 15:13:00 +0200

    Bug fixed.

--- main.py
+++ main.py
@@ -15,17 +15,3 @@ parser.startRule()
 print('Parsing done.')
 standardizer = parser.getStandardizer()
 standardizer.execute()
-
-
-
-
-#i = 1
-
-#while (token := lexer.nextToken()).type != sl.EOF:
-#    if token.channel != sl.HIDDEN:
-#        line = token.line
-#        charpositioninline = token.charPositionInLine
-#        type = sl.tokenNamesMap[token.type]
-#        text = token.text
-#        print('Token n. %d: line %d, charposition %d, type %s, text %s' %(i,line,charpositioninline,type,text))
-#        i+=1
